68d4b305469c61d74b8a6cbe52ed232c321a8f55
Kanaev <kanaev.aiu@phystech.edu>
2021-11-29 16:36:55 +0300

    Main adds

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
+from pygame.constants import K_RIGHT, K_UP
 from map_array.map import map_array
-from Core.object_core import objects
 from Core.draw_core import calculate_coard, map_compile
-
+from Player.player import Player
+from house_object.House import House
 import pygame
 
+player = Player()
 #screen = 1800 * 900
 WIDTH = 1800
 HEIGHT = 900
 WHITE = (255, 255, 255)
 FPS = 30
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-Player_x = 0
-Player_y = 0
+import pygame
+
+Map_objects = []
+
+for y_sector in range(30):
+    Building_line = []
+    for x_sector in range(46):
+        if map_array[y_sector, x_sector] == "Building":
+            Building_line.append(House(screen, 5))
+
+        
+
+
+
+
+for i in range(player.sector - 3, player.sector + 4):
+    for i in range(player.sector - 2, player.sector + 3):
+
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 clock = pygame.time.Clock()
-
+finished = False
 
 while not finished:
     screen.fill(WHITE)
+    pygame.display.update()
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,13 +50,18 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONUP:
             print()
         elif event.type == pygame.MOUSEMOTION:
-            print()
+            mouse_coard = event.pos
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_RIGHT:
+                print("a")
 
+    '''
     calculate_coard(objects, Player)
 
     for object_type in objects:
         for obj in object_type:
             obj.draw
+            '''
 
 
 
