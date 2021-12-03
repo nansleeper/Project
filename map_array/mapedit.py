9ab8d68f4c8fb18f362d9225e3e8cc9d8c691648
rubo20003 <karakhanian.rg@phystech.edu>
2021-12-03 05:44:26 +0300

    asas

--- map_array/mapedit.py
+++
@@ -1,8 +0,0 @@
-with open("mapinfo.txt", 'w') as map:
-    for k in range(31):
-        for i in range(47):
-            if k % 5 != 0 and i % 5 != 0:
-                print(k, i, " Building\n", file=map)
-                # map.write(k, " ", i, " Building\n")
-            else:
-                print(k, i, " Road\n", file=map)
