ba2250f303d12c452550f6633aaf0fc95ff92baf
MarZaitseva <zaitseva.ma@phystech.edu>
2021-12-06 22:07:42 +0300

    cssc

--- car/car.py
+++ car/car.py
@@ -1,6 +1,4 @@
-from random import choice
-from pygame.draw import *
-from random import randint
+
 import math
 import pygame
 import random
@@ -64,37 +62,87 @@ class Square(pygame.sprite.Sprite):
         self.color = (128, 128, 128)
         self.speed = 3
         self.angle = 0
+        self.hit = 0
+        self.t = 0 
+        self.n = 0
 
         self.side1 = 30
         self.side2 = 50
 
-        self.surface = pygame.Surface((self.side1, self.side2), pygame.SRCALPHA)
-        self.surface.set_colorkey((255,255,255))
+        self.surface = pygame.Surface((self.side1, self.side2), pygame.SRCALPHA,32)
+        self.surface.set_colorkey((255,255,255, 0))
         self.rect = self.surface.get_rect(center=(x, y))
+
 
     def update(self, win, n, orientation = 2, angle = 0, parametr = width):
       '''orientation- 1 горизонтально, 2 вертикально'''
-      self.angle = angle 
+      self.n = n
+      if self.hit == 1 and self.t < 1:
+
+        center = self.rect.center
+        image = pygame.transform.rotate(self.surface, -self.angle)
+        self.rect = image.get_rect()
+        self.rect.center = center
+        pygame.draw.rect(self.surface, self.color, (0,0, self.side1, self.side2))
+        win.blit(image, self.rect)
+
+        self.t += 0.01
+        print(self.t)
+
+      else:
+
+        self.angle = angle 
+        center = self.rect.center
+        if center[1] - self.side2/2 >= parametr and self.angle < 90 and n == 2:
+          self.angle = (self.angle + self.speed) % 360
+          #self.rect.x, self.rect.y = self.rect.y, self.rect.x
+          
+        image = pygame.transform.rotate(self.surface, self.angle)
+        self.rect = image.get_rect()
+        self.rect.center = center
+
+        if self.angle == 90 or orientation == 1:
+          self.rect.x += 1.5 * (-1)**n
+         
+        else:
+          self.rect.y += 1.5 * (-1)**n
+
+        if self.rect.top >= HEIGHT:
+          self.kill()
+
+        pygame.draw.rect(self.surface, self.color, (0,0, self.side1, self.side2))
+        win.blit(image, self.rect)
+
+      
+
+
+      '''else:
+        center = self.rect.center
+        image = pygame.transform.rotate(self.surface, self.angle)
+        self.rect = image.get_rect()
+        self.rect.center = center
+        pygame.draw.rect(self.surface, self.color, (0,0, self.side1, self.side2))
+        win.blit(image, self.rect)'''
+
+
+
+    def collisions(self, obj):
       center = self.rect.center
-      if center[1] - self.side2/2 >= parametr and self.angle < 90 and n == 2:
+      self.hit = 1
+      self.t = 0
+      if self.rect.x > obj.x:
         self.angle = (self.angle + self.speed) % 360
-        #self.rect.x, self.rect.y = self.rect.y, self.rect.x
-        
+
       image = pygame.transform.rotate(self.surface, self.angle)
       self.rect = image.get_rect()
       self.rect.center = center
 
-      if self.angle == 90 or orientation == 1:
-        self.rect.x += 1.5 * (-1)**n
-       
-      else:
-        self.rect.y += 1.5 * (-1)**n
-
-      if self.rect.top >= HEIGHT:
-        self.kill()
-
+      self.rect.x += (self.rect.x - obj.x)/abs(self.rect.x - obj.x) * 20 * math.sin(math.radians(360-self.angle)) * (-1)**n
+      self.rect.y -= (self.rect.y - obj.y)/abs(self.rect.y - obj.y) * 20 * math.cos(math.radians(360-self.angle)) * (-1)**n
       pygame.draw.rect(self.surface, self.color, (0,0, self.side1, self.side2))
       win.blit(image, self.rect)
+
+
 
 class Square_main(Square):
   def __init__(self, x, y):
@@ -185,7 +233,7 @@ class Square_main(Square):
           if self.start == 0:
             self.start = 1
             self.rect.center = (self.rect.x - int(pygame.Surface.get_width(self.surface)/2), self.rect.y - int(pygame.Surface.get_height(self.surface)/2))
-          print(self.start)
+         # print(self.start)
 
           
          
@@ -206,7 +254,7 @@ class Square_main(Square):
       if self.rect.top >= HEIGHT:
         self.kill()
 
-
+      #print(self.angle)
       pygame.draw.rect(self.surface, self.color, (0, 0, self.side1, self.side2))
       win.blit(image, self.rect)
 
@@ -215,12 +263,13 @@ class Square_main(Square):
     if pygame.sprite.collide_rect(obj, self):
       image = pygame.transform.rotate(self.surface, self.angle)
       self.rect = image.get_rect()
-      print(self.y, self.x, obj.rect.x, obj.rect.y)
-      self.x += (self.x - obj.rect.x)/abs(self.x - obj.rect.x) * 20 * math.sin(math.radians(360-self.angle))
-      self.y -= (self.y - obj.rect.y)/abs(self.y - obj.rect.y) * 20 * math.cos(math.radians(360-self.angle))
+      #print(self.rect.x, obj.rect.x, self.rect.y, obj.rect.y, self.x - obj.rect.x )
+      self.x += (self.x - obj.rect.x)/abs(self.x - obj.rect.x) * math.sin(math.radians(360-self.angle))
+      self.y -=(-1) * (self.y - obj.rect.y)/abs(self.y - obj.rect.y) * math.cos(math.radians(360-self.angle))
       self.rect.x = self.x
       self.rect.y = self.y
-      print(self.rect.y, self.rect.x, obj.rect.x, obj.rect.y)
+      self.rect.center = (self.rect.x - int(pygame.Surface.get_width(self.surface)/2), self.rect.y - int(pygame.Surface.get_height(self.surface)/2))
+      #print(self.rect.y, self.rect.x, obj.rect.x, obj.rect.y)
 
       pygame.draw.rect(self.surface, self.color, (0,0, self.side1, self.side2))
       win.blit(image, self.rect)
@@ -288,10 +337,7 @@ if __name__ == '__main__':
         road2.drawing(win)
         road3.drawing(win)
 
-        square_group1.update(win, 2, 1, 90)
-        square_group2.update(win, 1, 1, 90)
-        square_group3.update(win, 2)
-        square_group4.update(win, 1)
+        
         cars = square_group1.sprites() + square_group2.sprites() + square_group3.sprites() + square_group4.sprites()
         
 
@@ -334,9 +380,16 @@ if __name__ == '__main__':
          # print(type(car[0]))
         square_group_main.update(win, motion, n)
         for r in range(0,len(cars)):
-          car_main.collisions(cars[r])
-          if car_main.collisions(cars[r]):
+          car_main.collisions(cars[r]) 
+          if car_main.collisions(cars[r]) :
             square_group_main.update(win, motion, n)
+            cars[r].collisions(car_main)
+
+        square_group1.update(win, 2, 1, 90)
+        square_group2.update(win, 1, 1, 90)
+        square_group3.update(win, 2)
+        square_group4.update(win, 1)
+            
 
         #pygame.draw.rect(win, (30,30,30), (0, 0, WIDTH, HEIGHT), 8)
         clock.tick(FPS)
