900aa12cfa648626723c45d31719c1b8765ce83b
Kanaev <kanaev.aiu@phystech.edu>
2021-12-16 13:10:57 +0300

    Some messages for Masha

--- Core/carinstr.py
+++ Core/carinstr.py
@@ -1,6 +1,6 @@
 """
 class Car():
-    def __init__(globalcent):
+    def __init__(globalcent, textur):
         self.globalcent = globalcent - глобальные координаты (x, y)
         self.localcent = globalcent - player.coards - локальные координаты
         self.v - скорость
@@ -10,11 +10,13 @@ class Car():
         self.playerstatus - True если playerr управляет ею
         self.sector - сектор в глобальных координатах
         self.rotatetime - время, которое машина поворачивает
+        self.tex = textur
+        self.time = 0 - личное время для проведения некорых процессов
 
 
 
     def collisions(obj):
-        проверяет столкнулась ли машина с obj(другая машина)
+        проверяет столкнулась ли машина с obj(другая машина), домами(напиши мне, обсудим) (объект с центром в obj.cent и шириной obj.hitbox)
             *******
         self.hit = True
 
@@ -23,23 +25,29 @@ class Car():
         если есть - self.stop = True
 
     def rotate((x0, y0 , alpha0), (x1, y1, alpha1))
-        self.time += 1
-        машина за self.rotatetime плавно поворачивает из одного положения в другое
-        if self.rotatetime <
-
-    def move(delta_v_player):
-
-        if playerstatus:
-            if not (self.hit and self.stop):
-                пересчет координат по скоростям
-            else:
-                машина стоит
+        if self.rotatetime < 50
+            self.time += 1
+            машина за self.rotatetime = 50 плавно поворачивает из одного положения в другое
         else:
-            delta_v_player - массив из 2 чисел, изменением скоростей персонажа по стрелкам
-            преобразовать горизонтальные в изменение угла, а вертикальные в скорость (x, y)
+            self.time = 0
+
+    def move(delta_v_player, param1 = 0, param2 = 0):
+        if self.time != 0: 
+            self.rotate(param1, param2)
+        else:
+            self.collisions
+            self.stop
+            if playerstatus:
+                if not (self.hit and self.stop):
+                    пересчет координат по скоростям
+                else:
+                    машина стоит
+            else:
+                delta_v_player - массив из 2 чисел, изменением скоростей персонажа по стрелкам
+                преобразовать горизонтальные в изменение угла, а вертикальные в скорость (x, y)
             
-            if not self.hit:
-                пересчет координат
+                if not self.hit:
+                    пересчет координат
 
     
         
